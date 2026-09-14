d6cd926d52a6f774f88bde0db476ce4c3bc60b79
divzmadhu <divya.indi@gmail.com>
2026-09-14 07:00:19 -0700

    third commit with else if statement

--- script.js
+++ script.js
@@ -11,8 +11,20 @@ let age = 15;
 
 // Example if else statement
 
-if (age >= 18) {
-  console.log("You are eligible to vote.");
+// if (age >= 18) {
+//   console.log("You are eligible to vote.");
+// } else {
+//   console.log("You are not old enough to vote.");
+// }
+
+// Example else if Statement 
+
+let temperature = 25;
+ 
+if (temperature > 30) {
+  console.log("It's too hot!");
+} else if (temperature < 10) {
+  console.log("It's too cold!");
 } else {
-  console.log("You are not old enough to vote.");
+  console.log("The weather is just right.");
 }
