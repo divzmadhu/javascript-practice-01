771c68a2ed3c4b3913ac1ee8787aa3cdc3e70302
divzmadhu <divya.indi@gmail.com>
2026-09-14 06:49:19 -0700

    second commit with simple if

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let age = 18;
+// let age = 18;
+let age = 15;
+
+
+// Example Simple if statement
  
+// if (age >= 18) {
+//   console.log("You are eligible to vote.");
+// }
+// console.log('done');
+
+// Example if else statement
+
 if (age >= 18) {
   console.log("You are eligible to vote.");
+} else {
+  console.log("You are not old enough to vote.");
 }
