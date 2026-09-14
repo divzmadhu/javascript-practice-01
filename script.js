324133bdff68688111346d6e2b5839b454b735f3
divzmadhu <divya.indi@gmail.com>
2026-09-14 07:06:40 -0700

    Fourth commit with switch case

--- script.js
+++ script.js
@@ -19,12 +19,30 @@ let age = 15;
 
 // Example else if Statement 
 
-let temperature = 25;
+// let temperature = 25;
  
-if (temperature > 30) {
-  console.log("It's too hot!");
-} else if (temperature < 10) {
-  console.log("It's too cold!");
-} else {
-  console.log("The weather is just right.");
+// if (temperature > 30) {
+//   console.log("It's too hot!");
+// } else if (temperature < 10) {
+//   console.log("It's too cold!");
+// } else {
+//   console.log("The weather is just right.");
+// }
+
+// Example switch, break, and continue Statement 
+
+let choice = prompt("Choose an option: 1. Start 2. Settings 3. Exit");
+ 
+switch (choice) {
+  case '1':
+    console.log("Starting the game...");
+    break;
+  case '2':
+    console.log("Opening settings...");
+    break;
+  case '3':
+    console.log("Exiting the game...");
+    break;
+  default:
+    console.log("Invalid option, please try again.");
 }
